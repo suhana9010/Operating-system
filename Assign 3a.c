fa66950ef8172b7421d9c10531152afcb1a5685c
suhana9010 <61060971+suhana9010@users.noreply.github.com>
2020-08-26 03:11:51 -0700

    Create Assign 3a.c

---
+++ Assign 3a.c
@@ -0,0 +1,14 @@
+#include<stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h>   
+int main() 
+{   
+    int pid = fork(); 
+    if (pid > 0) 
+        printf("End of Parent"); 
+    else if (pid == 0) { 
+       sleep(20);
+        printf("End of child"); 
+    } 
+    return 0; 
+}
